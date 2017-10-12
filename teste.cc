9b73e905d5f4506d88034c44e61c9e0b6deacdee
ErikIPU2 <erik02_palmeiras@hotmail.com>
2017-10-12 16:22:15 -0300

    Sla mabi[

--- teste.cc
+++ teste.cc
@@ -2,16 +2,8 @@
 
 using namespace std;
 
-int** inversor(int vet[][]){
-	return vet;
-}
-
 int main()
 {
-	int vet[2][2] = {{1, 2},{3, 4}};
-
-	vet = inversor(vet);
-
-	return 0;
+	cout << "Que lixo isso";
 }
 
