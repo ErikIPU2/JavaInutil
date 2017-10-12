ae39fc8daa628516ccf38953afcbe274dee4f964
ErikIPU2 <erik02_palmeiras@hotmial.com>
2017-10-12 16:19:25 -0300

    Algo aleatoria que não é Java

---
+++ teste.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int** inversor(int vet[][]){
+	return vet;
+}
+
+int main()
+{
+	int vet[2][2] = {{1, 2},{3, 4}};
+
+	vet = inversor(vet);
+
+	return 0;
+}
+
